0c3e1a083f49961da13b3da15425f8ae94107299
Davi Manoel <davimanoel05@gmail.com>
2026-03-04 08:28:06 -0300

    calculadora simples

---
+++ calculadora/main.java
@@ -0,0 +1,93 @@
+package calculadora;
+
+import java.util.Scanner;
+
+public class main{
+
+    public void soma(Scanner sc){
+        long soma = 0, num = 1;   
+        System.out.println("Digite os valores. 0 para sair.");
+        while(sc.hasNext()){
+            num = sc.nextLong();
+            sc.nextLine();
+            if(num == 0) break;
+            soma += num;
+        }
+        System.out.println("Soma: " + soma + "\n");
+    }
+
+    public void subtracao(Scanner sc){
+        long sub = 0, num = 1;
+        System.out.println("Digite os valores. 0 para sair");
+        sub = sc.nextLong();
+        sc.nextLine();
+        while(sc.hasNext()){
+            num = sc.nextLong();
+            sc.nextLine();
+            if(num == 0) break;
+            sub -= num;
+        }
+        System.out.println("Subtracao: " + sub + "\n");
+    }
+
+    public void multiplicacao(Scanner sc){
+        long mult = 1, num = 1;
+        System.out.println("Digite os valores. 0 para sair");
+        while(sc.hasNext()){
+            mult = mult * num;
+            num = sc.nextLong();
+            sc.nextLine();
+            if(num == 0) break;
+        }
+        System.out.println("Multiplicacao: " + mult + "\n");
+    }
+
+    public void divisao(Scanner sc){
+        float div = 1, num = 1;
+        System.out.println("Digite os valores. 0 para sair");
+        div = sc.nextLong();
+        sc.nextLine();
+        while(sc.hasNext()){
+            div = div / num;
+            num = sc.nextLong();
+            sc.nextLine();
+            if(num == 0) break;
+        }
+        System.out.println("Divisao: " + div + "\n");
+    }
+
+    public static void main(String[] args){
+        String op = "";
+        Scanner sc = new Scanner(System.in);
+        main calculadora = new main();
+            while(!op.equals("0")){
+                System.out.println("Digite a operação: ");
+                System.out.println("SOMA: + | SUBTRACAO: - | MULTIPLICACAO: * | DIVISAO: / | SAIR: 0");
+                op = sc.nextLine();
+                switch (op) {
+                    case "+":
+                        calculadora.soma(sc);
+                        break;
+                    
+                    case "-":
+                        calculadora.subtracao(sc);
+                        break;
+
+                    case "*":
+                        calculadora.multiplicacao(sc);
+                        break;
+                
+                    case "/":
+                        calculadora.divisao(sc);
+                        break;
+
+                    case "0":
+                        break;
+                    default:
+                        System.out.println("Operacao invalida. Tente novamente.\n");
+                        break;
+                }
+            }
+        sc.close();
+    }
+}
